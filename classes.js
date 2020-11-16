68caa2ff46843bf82a8e99c387db626002066b80
Andrew Smith <topcodeandrew@gmail.com>
2020-11-16 16:34:40 -0700

    finished assignment

--- classes.js
+++ classes.js
@@ -29,7 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age 
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +57,19 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee){
+    return this.reports.push(employee);
+  }
+
+  fire(index){
+    return this.reports.splice(index,1);
+  }  
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,12 +88,69 @@
     51-100 reports : Manager Plus
     101+ reports : Bestest Manager
 
-  Everytime they fire an employee they get $100 added to their bonus.
+  Every time they fire an employee they get $100 added to their bonus.
 
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age, reports){
+    super (first_name, last_name, email, age, reports)
+    this.title = "Not a manager"
+    this.bonus = 0
+  }
+
+  updateTitle() {
+    if(this.reports.length === 0) {
+      this.title = "Not a manager"
+      return this.title;
+    }
+    else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = "Barely Manager"
+      return this.title;
+    }
+    else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = "Mostly Manager"
+      return this.title;
+    }
+    else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = "Manager"
+      return this.title;
+    }
+    else if(this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = "Manager Plus"
+      return this.title;
+    }
+    else if (this.reports.length >= 101) {
+      this.title = "Bestest Manager"
+      return this.title;
+    } 
+    
+    
+  }
+  hire(employee){
+    super.hire(employee)
+    this.updateTitle()    
+  }
+
+  fire(index){
+    super.fire(index)
+    this.updateTitle()
+    this.bonus += 100
+  }  
+  
+}
+
+// console.log(ProgressiveManager)
+
+// let John = new ProgressiveManager("John", "Doe", "email@email", 32, "frank")
+// console.log(John)
+
+
+
+
+
+
 
 
 
@@ -98,6 +177,17 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machines {
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+
+  makeWidgets(number){
+    this.widgets_made_count += number;
+  }
+
+}
 
 
--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+let inner = outer()
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner()
 
 
 
@@ -51,7 +51,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend("Jake")
 
 
 
@@ -61,16 +61,24 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  let number = 0
+
+  return function(){
+    number ++
+    return number
+  }
+
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -85,19 +93,26 @@ function callFriend(name) {
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
-function counterFactory(value) {
+function counterFactory(num) {
   // Code here.
 
   return {
+    inc: () => {
+      return num += 1
+    },
+
+    dec: () => {
+      return num -= 1
+    }
 
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,10 +127,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message(){
+    return `You're doing awesome, keep it up ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,11 +160,11 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: () => privateMethod()
   };
 })();
 
-
+module.publicMethod()
 
 ////////// PROBLEM 7 //////////
 
@@ -162,7 +179,13 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: (num) => {
+      return secret += num
+    },
+
+    takeAwayFromSecret: (num2) => {
+      return secret -= num2
+    }
   };
 }
 
@@ -188,9 +211,13 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+
+    function outer(index) {
+      setTimeout(function() {
+        console.log(index);
+      }, index * 1000);
+    }
+    outer(i)
   }
 }
 timeOutCounter();
